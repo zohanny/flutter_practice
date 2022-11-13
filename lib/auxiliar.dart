4fe05432f420f8e8207a6cb75b4fcea56252d02c
queiros <queiros.joao@gmail.com>
2022-11-13 21:47:44 +0000

    Three Files Structure - After Tutorial #9 (Images)

--- lib/auxiliar.dart
+++ lib/auxiliar.dart
@@ -1,3 +1,36 @@
+//This file would be a backend method
+//There is no styles here, only returns raw data to be styled in the frontend. (home_file.dart)
+
+import 'package:flutter/material.dart';
+
 String getTextForApp() {
-  return "Hello my friend";
+  String testReturned = "This text is returned from the getTextForApp() method from the auxiliar.dart file";
+  return testReturned;
 }
+
+String getImageURL() {
+  String urlImage = "https://www.pariscityvision.com/pt/paris/museus-de-paris/museu-do-louvre/mona-lisa-historia-misterios";
+  return urlImage;
+}
+
+AssetImage getAssetImage(int numero) {
+  AssetImage ass;
+  if (numero == 1)
+    ass =  const AssetImage('assets/spacee-scaled.jpg');
+  else
+    ass =  const AssetImage('assets/deep.jpg');
+  return ass;
+}
+
+/*
+Text(
+getTextForApp(), // THIS LINE EXECUTES THE METHOD FROM THE AUXILIAR.DART FILE
+style: TextStyle(
+fontSize: 25,
+fontWeight: FontWeight.bold,
+letterSpacing: 2,
+color: Colors.grey[600],
+fontFamily: 'IndieFlower',
+
+),
+*/
--- lib/home_file.dart
+++ lib/home_file.dart
@@ -1,35 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/auxiliar.dart';
 
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Yey Great stuff'),
+        title: const Text('Flutter app'),
         centerTitle: true,
-        backgroundColor: Colors.blue[600],
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          getTextForApp(), // THIS LINE EXECUTES THE METHOD FROM THE AUXILIAR.DART FILE
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-
-          ),
+        child: Image(
+            image: getAssetImage(1), // o Url/Asset da imagem vem do file auxiliar.dart
+            //Image.asset('assets/deep.jpg') // shortcut
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Text("Click"),
-        onPressed: () {},
+        onPressed: click(),
         backgroundColor: Colors.red[600],
       ),
     );
   }
 
-
+  click() {}
 }
