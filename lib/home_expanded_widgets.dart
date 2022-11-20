51a7acd6e59d8d5beeba131387c2ad0f465b2977
queiros <queiros.joao@gmail.com>
2022-11-20 15:18:45 +0000

    Expanded Widget after tut #14

---
+++ lib/home_expanded_widgets.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+//import 'package:myapp/auxiliar.dart';
+
+
+class Home_exp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        children: [
+          Expanded(
+              flex:6,
+              child: Image.asset('assets/deep.jpg')),
+          Expanded(
+            flex:2,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: Text('2'),
+                  ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('3'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: click(),
+        backgroundColor: Colors.red[600],
+        child: const Text("Click"),
+      ),
+    );
+  }
+
+  click() {}
+}
